7b748189c70e15307760119da748264dfd6c9eca
sbhutta123 <shifa.bhutta33@gmail.com>
2026-03-18 17:34:51 -0400

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
 
   // If your repo is github.com/USERNAME/march-madness, set basePath:
   basePath: "/march-madness",
-  assetPrefix: "/march-madness/",
+  assetPrefix: "/march-madness",
   // Uncomment the two lines above and replace "march-madness" with your repo name.
 };
 
